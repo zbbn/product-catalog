3eaa73dd706a8079708d62c2cb6465cb3cacf1ca
zbbn <sebastian.e.andersson@gmail.com>
2026-07-05 21:51:43 +0200

    chore: fixed Billy URL

--- backend/src/data/mockProducts.ts
+++ backend/src/data/mockProducts.ts
@@ -8,6 +8,6 @@ export const mockProducts: Product[] = [
         category: 'Storage',
         description: 'A classic adjustable bookcase that fits almost any room.',
         price: 59.99,
-        imageUrl: 'https://picsum.photos/seed/billy/400/300',
+        imageUrl: 'https://www.ikea.com/se/sv/p/billy-bokhylla-vit-00263850/'
     },
 ]
